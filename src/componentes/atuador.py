7bbc1cd09b294f992b6528b7a5f64e1e6f9600c1
leviAnastacio <levianastacio7.0@gmail.com>
2024-05-24 21:34:06 -0300

    Métodos do atuador e instanciações dos atuadores

--- src/componentes/atuador.py
+++ src/componentes/atuador.py
@@ -1,10 +1,24 @@
 class Atuador:
-    def __init__(self, id, onOff):
-        self.id = id
+    def __init__(self, onOff, id):
         self.onOff = onOff
+        self.id = id
+        
+    def ligar(self, ligar):
+        if(ligar == True and self.onOff == False):
+            self.onOff = True
+        else:
+            pass
+    
+    def Desligar(self, Desligar):
+        if(Desligar == True and self.onOff == True):
+            self.onOff = True
+        else:
+            pass
+            
+            
+        
+Aquecedor = Atuador(False, 1)
+resfriador = Atuador(False, 2)
+irrigador = Atuador(False, 3)
+injetorCo2 = Atuador(False, 4)
 
-
-
-#temperatura min 16 max 22
-#umidade min 50% max 80% 
-#min 1200ppm max 1500ppm
